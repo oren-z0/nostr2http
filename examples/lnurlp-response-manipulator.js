975f4d50f79b1ce3994bc797f46a7a713345868c
Oren <orenz0@protonmail.com>
2024-04-27 23:04:12 +0300

    explaining manipulator

--- examples/lnurlp-response-manipulator.js
+++ examples/lnurlp-response-manipulator.js
@@ -1,3 +1,14 @@
+// Manipulates payRequest json responses. If the callback url domain is the same as the
+// destination (i.e. it's an ip address in our local network and is not accessible from outside),
+// then we change the callback to begin with 'nostr1+http://<server-npub>/'. This way the
+// client will know that the second request should also be sent to us over nostr.
+// For example, if you are running LNbits with the lnurlp extension on you home network (i.e
+// behind NAT), download this file to a local folder in your Umbrel machine and run the following
+// command:
+// npx nostr2http --relays wss://relay.damus.io wss://nos.lol wss://relay.snort.social \
+//  --nsec-file my-nsec.txt --save-nsec --allowed-routes '/lnurlp/**' --response-manipulator-file
+//  lnurlp-response-manipulator.js http://127.0.0.1:3007
+
 module.exports = ({ responseMessage, keys, destination }) => {
   if (responseMessage.headers['content-type'] !== 'application/json') {
     return;
@@ -23,5 +34,5 @@ module.exports = ({ responseMessage, keys, destination }) => {
       ...responseBody,
       callback: `nostr1+http://${keys.publicKey.toBech32()}${responseBody.callback.slice(destination.length)}`
     }))
-  }
+  };
 };
