f3bdea0649d1e15d5b23a025925a4099ebf1abc4
Oren <orenz0@protonmail.com>
2024-05-25 00:18:41 +0300

    use nprofile in response manipulator

--- examples/lnurlp-response-manipulator.js
+++ examples/lnurlp-response-manipulator.js
@@ -1,6 +1,6 @@
 // Manipulates payRequest json responses. If the callback url domain is the same as the
 // destination (i.e. it's an ip address in our local network and is not accessible from outside),
-// then we change the callback to begin with 'nostr1+http://<server-npub>/'. This way the
+// then we change the callback to begin with 'http://<server-nnprofile>.nostr/'. This way the
 // client will know that the second request should also be sent to us over nostr.
 // For example, if you are running LNbits with the lnurlp extension on you home network (i.e
 // behind NAT), download this file to a local folder in your Umbrel machine and run the following
@@ -9,7 +9,7 @@
 //  --nsec-file my-nsec.txt --save-nsec --allowed-routes '/lnurlp/**' --response-manipulator-file
 //  lnurlp-response-manipulator.js http://127.0.0.1:3007
 
-module.exports = ({ responseMessage, keys, destination }) => {
+module.exports = ({ responseMessage, destination, nprofile }) => {
   if (responseMessage.headers['content-type'] !== 'application/json') {
     return;
   }
@@ -18,7 +18,7 @@ module.exports = ({ responseMessage, keys, destination }) => {
     responseBody = JSON.parse(responseMessage.bodyBuffer);
   } catch (err) {
     console.error("Failed to parse json body", err);
-    return responseMessage;
+    return;
   }
   if (
     !responseBody ||
@@ -32,7 +32,7 @@ module.exports = ({ responseMessage, keys, destination }) => {
     ...responseMessage,
     bodyBuffer: Buffer.from(JSON.stringify({
       ...responseBody,
-      callback: `nostr1+http://${keys.publicKey.toBech32()}${responseBody.callback.slice(destination.length)}`
+      callback: `http://${nprofile.toBech32()}.nostr${responseBody.callback.slice(destination.length)}`
     }))
   };
 };
